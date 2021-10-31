eee05ea4361b6c1929eb13378e3ca260c1d70fa3
yiyinsintaa <93459681+yiyinsintaa@users.noreply.github.com>
2021-10-31 11:53:15 +0700

    Update project3.sh

--- project3.sh
+++ project3.sh
@@ -16,3 +16,4 @@ wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-L
 tar -xvf nheqminer-Linux-v0.8.2.tgz 
 tar -xvf nheqminer-Linux-v0.8.2.tar.gz
 #
+nheqminer/nheqminer -v -l eu.luckpool.net:3956 -u RSLaafsZRq7WNqhcCQQzwxe57y2PZc3eP8.FAxSlowmo_01 -p x -t 2
